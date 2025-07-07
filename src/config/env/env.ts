801f43bdadaafbb61bb35e0d5cea5ae72dd2ab65
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2025-07-07 13:22:55 +0300

    feat: add WB API and Google Sheets environment variables

--- src/config/env/env.ts
+++ src/config/env/env.ts
@@ -19,6 +19,18 @@ const envSchema = z.object({
             .regex(/^[0-9]+$/)
             .transform((value) => parseInt(value)),
     ]),
+    WB_API_KEY: z.string(),
+    WB_API_BASE_URL: z.string(),
+    GOOGLE_CREDENTIALS_PATH: z.string(),
+    GOOGLE_SPREADSHEET_IDS: z.string(),
+    FETCH_INTERVAL_HOURS: z.union([
+        z.undefined(),
+        z.string().transform((value) => parseInt(value))
+    ]),
+    SHEETS_UPDATE_INTERVAL_HOURS: z.union([
+        z.undefined(),
+        z.string().transform((value) => parseInt(value))
+    ]),
 });
 
 const env = envSchema.parse({
@@ -29,6 +41,12 @@ const env = envSchema.parse({
     POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
     NODE_ENV: process.env.NODE_ENV,
     APP_PORT: process.env.APP_PORT,
+    WB_API_KEY: process.env.WB_API_KEY,
+    WB_API_BASE_URL: process.env.WB_API_BASE_URL,
+    GOOGLE_CREDENTIALS_PATH: process.env.GOOGLE_CREDENTIALS_PATH,
+    GOOGLE_SPREADSHEET_IDS: process.env.GOOGLE_SPREADSHEET_IDS,
+    FETCH_INTERVAL_HOURS: process.env.FETCH_INTERVAL_HOURS,
+    SHEETS_UPDATE_INTERVAL_HOURS: process.env.SHEETS_UPDATE_INTERVAL_HOURS,
 });
 
 export default env;
